e9275a1c4123efb82f69a0f0fb13c89e4619cf94
Christian Virgen <c.camilo.virgen@gmail.com>
2018-05-10 01:06:49 -0400

    functions and exceptions
    more basic concepts for python

---
+++ functions.py
@@ -0,0 +1,17 @@
+def hows_the_parrot():
+    print("He's pining for the fjords!")
+
+hows_the_parrot()
+
+def lumberjack(name, pronoun):
+    print("{}'s a lumberjack and {} ok!".format(name, pronoun))
+
+lumberjack('kenneth', "he's")
+
+
+def average(num1, num2):
+    return(num1+num2) / 2
+
+avg = average(2,8)
+print(average)
+
