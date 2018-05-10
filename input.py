75bd05398ca2295cefbf5b1a7cca570193fc54bb
Christian Virgen <c.camilo.virgen@gmail.com>
2018-05-10 00:02:04 -0400

    infinity loop
    some concepts including loops

---
+++ input.py
@@ -0,0 +1,16 @@
+age = int(input("whats your age?: "))
+
+if age <= 30:
+    print("your are still young so continue learning")
+else:
+    print("you're still alive so continue learning")
+
+
+my_List = ['python', 'java', 'javaScript', 'html', 'css']
+for skills in my_List:
+        print("I am getting better at: ", skills)
+
+
+alive = True
+while alive:
+    print("keep learning")
