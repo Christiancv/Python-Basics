23349ad9b5bafa20ac9fb549d67d54d1162c232e
Christian Virgen <c.camilo.virgen@gmail.com>
2018-05-16 00:13:30 -0400

    removes vowels form strings
    takes a string and removes vowels

--- introducing list/disemvowel.py
+++ introducing list/disemvowel.py
@@ -1,18 +1,13 @@
 def disemvowel(word):
-    result = []
-    for letter in word:
-        result.append(letter.lower())
-    try:
-        result.remove("a")
-        result.remove("e")
-        result.remove("i")
-        result.remove("o")
-        result.remove("u")
-    except ValueError:
-        pass
-    else:
+    letters = list(word)
+    bad = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+    for letter in bad:
+        if letter in letters:
+            letters.remove(letter)
+
+    disvowl = "".join(letters)
+    return disvowl
 
 
-
-
-disemvowel("halo")
+word = disemvowel("hIatO")
+print(word)
